f155010847180d39f84b1eea244e9b52cbcab57f
Kenneth <Kenneth@172.17.0.79>
2016-09-28 16:52:38 +0800

    Classes and Thread

--- src/com/teampurado/model/thread/TimerThread.java
+++ src/com/teampurado/model/thread/TimerThread.java
@@ -1,9 +1,56 @@
 package com.teampurado.model.thread;
 
+import com.teampurado.view.student.ReportFrame;
+import javax.swing.JLabel;
+
 /**
  *
  * @author ProfessorSci
  */
-public class TimerThread {
+public class TimerThread extends Thread {
     
+    private JLabel h;
+    private JLabel m;
+    private JLabel s;
+
+    public TimerThread(JLabel h, JLabel m, JLabel s) {
+        this.h = h;
+        this.m = m;
+        this.s = s;
+    }
+
+    public void run() {
+        byte hr = Byte.parseByte(h.getText()), 
+                min = Byte.parseByte(m.getText()), 
+                    sec = Byte.parseByte(s.getText());
+
+        try {
+            do {
+                h.setText("" + hr);
+                m.setText("" + min);
+                s.setText("" + sec);
+
+                Thread.sleep(1000);
+
+                if(sec > -1) {
+                    sec--;
+                } else if (min > -1){
+                    sec = 59;
+                    if(min > -1) {
+                        min--;
+                    } else {
+                        min = 59;
+                        if(hr > 0) {
+                            hr--;
+                        }
+                    }
+                }
+            } while(h.getText().equals("0") && m.getText().equals("0") && s.getText().equals("0"));
+            throw new InterruptedException();
+        } catch(InterruptedException ex) {
+            ReportFrame direct = new ReportFrame();
+            this.interrupt();
+        }
+    }
+
 }
