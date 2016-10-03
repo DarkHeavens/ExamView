d9877cb96265ea9b157b1dbdb7831170ff107ea1
Kenneth <Kenneth@172.17.2.209>
2016-10-04 02:13:26 +0800

    DBHelper

--- src/com/teampurado/model/database/DBHelper.java
+++ src/com/teampurado/model/database/DBHelper.java
@@ -42,43 +42,47 @@ public class DBHelper {
             execute("create table if not exists "+SUBJECT+
                     " (code varchar(20) not null,"+
                     " description varchar(50),"+
-                    " primary key(id))");
+                    " primary key(code))");
             
             execute("create table if not exists "+SUBJECT_TEACHER+
                     " (teacherID char(10),"+
                     " subjectCode varchar(20),"+
-                    " day varchar(8),"+
-                    " time varchar(60),"+
-                    " primary key(teacherID,subjectID),"+
-                    " foreign key(teacherID) references examview.teacher(id),"+
-                    " foreign key(subjectCode) references examview.subject(code))");
+                    " day varchar(8) not null,"+
+                    " time varchar(60 not null),"+
+                    " primary key(teacherID,subjectCode),"+
+                    " foreign key(teacherID) references examview.teacher(id) on delete cascade on update cascade,"+
+                    " foreign key(subjectCode) references examview.subject(code) on delete cascade on update cascade)");
             
             execute("create table if not exists "+EXAM+
-                    " (examID tinyint not null auto_increment,"+
-                    " teacherID char(10),"+
-                    " subjectCode varchar(20),"+
+                    " (examID tinyint null auto_increment,"+
+                    " teacherID char(10) not null,"+
+                    " subjectCode varchar(20) not null,"+
                     " numOfItems smallint,"+
                     " timeLimit char(8),"+
                     " password varchar(126),"+
+                    " status boolean not null,"+
                     " primary key(examID),"+
-                    " foreign key(teacherID) references examview.teacher(id),"+
-                    " foreign key(subjectCode) references examview.subject(code))");
+                    " foreign key(teacherID) references examview.teacher(id) on delete cascade on update cascade,"+
+                    " foreign key(subjectCode) references examview.subject(code) on delete cascade on update cascade)");
             
             execute("create table if not exists "+QUESTION_BANK+
                     " (QBankID int not null,"+
-                    " examID tinyint,"+
+                    " examID tinyint null,"+
+                    " teacherID char(10) not null,"+
+                    " description text unique not null,"+
                     " primary key(QBankID),"+
-                    " foreign key(examID) references examview.exam(examID))");
+                    " foreign key(examID) references examview.exam(examID) on delete set null on update cascade,"+
+                    " foreign key(teacherID) references examview.teacher(id) on delete cascade on update cascade)");
             
             execute("create table if not exists "+QUESTION+
                     " (questionNo smallint not null auto_increment,"+
-                    " QBankID int,"+
+                    " QBankID int not null,"+
                     " numOfPoints tinyint,"+
-                    " ask text"+
+                    " ask text,"+
                     " answer text,"+
                     " choices text,"+
                     " primary key(questionNo),"+
-                    " foreign key(QBankID) references examview.question_bank(QBankID))");
+                    " foreign key(QBankID) references examview.question_bank(QBankID) on delete cascade on update cascade)");
             
             execute("create table if not exists "+STUDENT+
                     " (id char(10) not null,"+
@@ -88,20 +92,19 @@ public class DBHelper {
             
             execute("create table if not exists "+ATTEMPT+
                     " (studentID char(10) not null,"+
-                    " examID tinyint,"+
-                    " startTime char(8),"+
-                    " endTime char(8),"+
+                    " examID tinyint null,"+
+                    " status boolean not null,"+
                     " primary key(studentID,examID),"+
-                    " foreign key(studentID) references examView.student(id),"+
-                    " foreign key(examID) references examView.exam(examID))");
+                    " foreign key(studentID) references examView.student(id) on delete cascade on update cascade,"+
+                    " foreign key(examID) references examView.exam(examID) on delete set null on update cascade)");
             
             execute("create table if not exists "+REPORT+
-                    " (examID tinyint not null,"+
-                    " studentID char(10),"+
+                    " (studentID char(10) not null,"+
+                    " examID tinyint null,"+
                     " totalScore int,"+
-                    " primary key(examID),"+
-                    " foreign key(examID) references examView.exam(examID),"+
-                    " foreign key(studentID) references examView.student(id))");    
+                    " primary key(studentID),"+
+                    " foreign key(examID) references examView.exam(examID) on delete set null on update cascade,"+
+                    " foreign key(studentID) references examView.student(id) on delete cascade on update cascade)");    
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
@@ -111,57 +114,60 @@ public class DBHelper {
     }
     
     public void add(Attempt obj) {
-        executeQuery("insert into "+ATTEMPT+" values ("+
-                obj.getStudentID()+","+
+        executeQuery("insert into "+ATTEMPT+" values ('"+
+                obj.getStudentID()+"',"+
                 obj.getExamID()+","+
-                obj.getStartTime()+","+
-                obj.getEndTime()+")");
+                obj.isStatus()+")");
     }
     
     public void add(Exam obj) {
         execute("insert into "+EXAM+" values ("+
-                obj.getExamID()+","+
-                obj.getTeacherID()+","+
-                obj.getSubjectID()+","+
-                obj.getNumOfItems()+","+
-                obj.getTimeLimit()+","+
-                obj.getPassword()+")");
+                obj.getExamID()+",'"+
+                obj.getTeacherID()+"','"+
+                obj.getSubjectID()+"',"+
+                obj.getNumOfItems()+",'"+
+                obj.getTimeLimit()+"','"+
+                obj.getPassword()+"',"+
+                obj.isStatus()+")");
     }
     
     public void add(Question obj) {
         execute("insert into "+QUESTION+" values ("+
                 obj.getQuestionNo()+","+
-                obj.getqBankID()+","+
-                obj.getNumOfPoints()+","+
-                obj.getAnswer()+","+
-                obj.getChoices()+")");
+                obj.getQBankID()+","+
+                obj.getNumOfPoints()+",'"+
+                obj.getAsk()+"','"+
+                obj.getAnswer()+"','"+
+                obj.getChoices()+"')");
     }
     
     public void add(QuestionBank obj) {
         execute("insert into "+QUESTION_BANK+" values ("+
                 obj.getQBankID()+","+
-                obj.getExamID()+")");
+                obj.getExamID()+",'"+
+                obj.getTeacherID()+"','"+
+                obj.getDescription()+"')");
     }
     
     public void add(Report obj) {
         execute("insert into "+REPORT+" values ("+
-                obj.getExamID()+","+
-                obj.getStudentID()+","+
+                obj.getExamID()+",'"+
+                obj.getStudentID()+"',"+
                 obj.getTotalScore()+")");
     }
     
     public void add(Subject obj) {
-        execute("insert into "+SUBJECT+" values ("+
-                obj.getCode()+","+
-                obj.getDescription()+")");
+        execute("insert into "+SUBJECT+" values ('"+
+                obj.getCode()+"','"+
+                obj.getDescription()+"')");
     }
     
     public void add(SubjectTeacher obj) {
-        execute("insert into "+SUBJECT_TEACHER+" values ("+
-                obj.getTeacherID()+","+
-                obj.getSubjectCode()+","+
-                obj.getDay()+","+
-                obj.getTime()+")");
+        execute("insert into "+SUBJECT_TEACHER+" values ('"+
+                obj.getTeacherID()+"','"+
+                obj.getSubjectCode()+"','"+
+                obj.getDay()+"','"+
+                obj.getTime()+"')");
     }
     
     public final boolean execute(String sql) {
